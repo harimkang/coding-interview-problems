9a5872e92c95be5c01dfef5ded2cbc3d211e407d
Harim Kang <harimkang4422@gmail.com>
2020-10-05 10:06:21 +0900

    2020-10-05 Coding-Test Problem Delete

--- etc/kakao_20200912/pro_5.py
+++
@@ -1,7 +0,0 @@
-def solution(play_time, adv_time, logs):
-    answer = ''
-    return answer
-
-
-if __name__ == "__main__":
-    pass
--- etc/line_20200913/pro_4.py
+++
@@ -1,8 +0,0 @@
-def solution(maze):
-    answer = 0
-    return answer
-
-
-
-if __name__ == "__main__":
-    pass
